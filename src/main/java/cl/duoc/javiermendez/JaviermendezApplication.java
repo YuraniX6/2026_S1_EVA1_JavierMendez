9afc45f3034ba1be362fa7ce5daa851d72111cdf
YuraniX6 <javi.mendeza@duocuc.cl>
2026-03-31 10:26:38 -0300

    first commit

---
+++ src/main/java/cl/duoc/javiermendez/JaviermendezApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.javiermendez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaviermendezApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JaviermendezApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/duoc/javiermendez/JaviermendezApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.javiermendez;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JaviermendezApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
