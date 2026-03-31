b2f36de715fe3afe705fa71c2c5ba3240e450f8b
YuraniX6 <Javi.mendeza@duocuc.cl>
2026-03-31 10:57:02 -0300

    creacion de modelo con parametros

--- src/main/java/cl/duoc/javiermendez/model/Paciente.java
+++ src/main/java/cl/duoc/javiermendez/model/Paciente.java
@@ -1,5 +1,23 @@
 package cl.duoc.javiermendez.model;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Paciente {
+
+    //Parametros del paciente
+
+    private int rut;
+    private String rutFinal;
+    private String nombre;
+    private String apellido;
+    private String fechaDeIngreso;
+    private String direccion;
+    private int telefono;
+    private String tipoAtencion;
+    private String nivelPrioridad;
 
 }
